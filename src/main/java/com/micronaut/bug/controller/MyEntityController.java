8b4a8bb3ebefafb631ebf8a9ba410b3c2c425392
altro3 <alexuyt@gmail.com>
2023-11-10 15:56:36 +0700

    Reproduce serde bug

--- src/main/java/com/micronaut/bug/controller/MyEntityController.java
+++ src/main/java/com/micronaut/bug/controller/MyEntityController.java
@@ -1,34 +1,59 @@
 package com.micronaut.bug.controller;
 
-import com.micronaut.bug.service.MyEntityService;
-import io.micronaut.http.MediaType;
+import java.math.BigDecimal;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import lombok.RequiredArgsConstructor;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.client.BlockingHttpClient;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
+import com.micronaut.bug.api.Animal;
+import com.micronaut.bug.api.Bird;
+import com.micronaut.bug.api.ColorEnum;
+import com.micronaut.bug.service.MyEntityService;
 
 @Slf4j
-@RequiredArgsConstructor
-//@Controller
+@Controller
 public class MyEntityController {
 
     private final MyEntityService entityService;
+    private final BlockingHttpClient httpClient;
 
-    @Get(value = "/load", produces = MediaType.TEXT_HTML)
-    public String test() {
-        entityService.loadFromDb();
-        return "OK!";
+    public MyEntityController(MyEntityService entityService, @Client("local") HttpClient httpClient) {
+        this.entityService = entityService;
+        this.httpClient = httpClient.toBlocking();
     }
 
-    @Get(value = "/save", produces = MediaType.TEXT_HTML)
-    public String save() {
-        entityService.save();
-        return "OK!";
+    @Post("/test")
+    public Animal test(@Body Animal animal) {
+        return animal;
     }
 
-    @Get(value = "/props", produces = MediaType.TEXT_HTML)
-    public List<String> getPropertiesBykey() {
-        return entityService.getPropertiesByKey("key1");
+    @Get("/start")
+    public Animal start() {
+
+        var bird = new Bird()
+                .numWings(2)
+                .beakLength(BigDecimal.valueOf(12, 1))
+                .featherDescription("Large blue and white feathers")
+                .color(ColorEnum.BLUE);
+
+//        var mammal = new Mammal(20.5f, "A typical Canadian beaver")
+//                .color(ColorEnum.BLUE);
+//
+//        var reptile = new Reptile(0, true)
+//                .fangDescription("A pair of venomous fangs")
+//                .color(ColorEnum.BLUE);
+
+
+        var resposne = httpClient.retrieve(HttpRequest.POST("/test", bird), Animal.class);
+        log.info("Response: {}", resposne);
+        return resposne;
     }
 }
