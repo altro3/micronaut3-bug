9b7eacdd75fc920ffaf6ecf9b2fae005523f846a
altro3 <alexuyt@gmail.com>
2024-02-15 16:02:40 +0700

    Test for serde

--- src/main/kotlin/com/micronaut/bug/Application.kt
+++ src/main/kotlin/com/micronaut/bug/Application.kt
@@ -1,28 +1,14 @@
 package com.micronaut.bug
 
-import io.micronaut.context.ApplicationContextBuilder
-import io.micronaut.context.ApplicationContextConfigurer
-import io.micronaut.context.annotation.ContextConfigurer
 import io.micronaut.runtime.Micronaut
-import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import io.swagger.v3.oas.annotations.info.Info
 
-@OpenAPIDefinition(info = Info(title = "broken-micronaut-openapi-expand", version = "sdsd", description = ""))
-@ContextConfigurer
-class Application : ApplicationContextConfigurer {
-    override fun configure(builder: ApplicationContextBuilder) {
-        println("Java configurer loaded")
-        builder.deduceEnvironment(false)
-                .banner(false)
-                .eagerInitConfiguration(true)
-                .eagerInitSingletons(true)
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build(*args)
                 .defaultEnvironments("local")
-    }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            Micronaut.run(Application::class.java, *args)
-        }
+                .banner(false)
+                .start()
     }
 }
--- src/main/kotlin/com/micronaut/bug/config/TestProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.micronaut.bug.config
-
-import io.micronaut.context.annotation.ConfigurationInject
-import io.micronaut.context.annotation.ConfigurationProperties
-
-@ConfigurationProperties("testing")
-data class TestProperties @ConfigurationInject constructor(
-    val enabled: Boolean = true
-)
