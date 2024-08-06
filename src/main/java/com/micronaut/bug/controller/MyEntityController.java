8f8f6c77c44b8a97f92fb99ff31ac36856bf1620
altro3 <alexuyt@gmail.com>
2024-08-06 17:16:02 +0700

    Update versions

--- src/main/java/com/micronaut/bug/controller/MyEntityController.java
+++ src/main/java/com/micronaut/bug/controller/MyEntityController.java
@@ -2,10 +2,13 @@ package com.micronaut.bug.controller;
 
 import java.math.BigDecimal;
 
+import com.example.openapi.api.DefaultApi;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Head;
+import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
@@ -14,6 +17,7 @@ import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
@@ -29,15 +33,26 @@ public class MyEntityController {
 
     private final MyEntityService entityService;
     private final BlockingHttpClient httpClient;
+    private final DefaultApi defaultApi;
 
-    public MyEntityController(MyEntityService entityService, @Client("local") HttpClient httpClient) {
+    public MyEntityController(MyEntityService entityService, @Client("local") HttpClient httpClient, DefaultApi defaultApi) {
         this.entityService = entityService;
         this.httpClient = httpClient.toBlocking();
+        this.defaultApi = defaultApi;
     }
 
-    @Post("/test")
-    public Mono<Animal> test(@Body @Valid Animal animal) {
-        return Mono.just(animal);
+    @Head("/")
+    void fileAvailable(
+            @Header("X-TEST-crc32") @NotNull String xtESTCrc32,
+            @Header("X-TEST-sha512") @NotNull String xtESTSha512,
+            @Header("X-TEST-size") @NotNull Long xtESTSize
+    ) {
+        log.info("xtESTCrc32: {}", xtESTCrc32);
+    }
+
+    @Get("/test")
+    public void test() {
+        defaultApi.fileAvailable("test1", "test2", 123L);
     }
 
     @Get("/start")
