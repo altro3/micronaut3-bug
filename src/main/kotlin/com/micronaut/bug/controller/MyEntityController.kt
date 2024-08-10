50251c9766a544c281e741afcd59494c706af14d
altro3 <alexuyt@gmail.com>
2024-08-10 13:20:12 +0700

    Update versions

--- src/main/kotlin/com/micronaut/bug/controller/MyEntityController.kt
+++ src/main/kotlin/com/micronaut/bug/controller/MyEntityController.kt
@@ -1,27 +1,17 @@
 package com.micronaut.bug.controller
 
-import com.micronaut.bug.api.Animal
-import com.micronaut.bug.api.Bird
-import com.micronaut.bug.api.ColorEnum
-import io.micronaut.http.HttpRequest
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.*
 import io.micronaut.http.client.BlockingHttpClient
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
-import jakarta.validation.Valid
-import jakarta.validation.constraints.NotNull
-import reactor.core.publisher.Mono
-import java.math.BigDecimal
+import jakarta.annotation.Nullable
 
 @ExecuteOn(TaskExecutors.BLOCKING)
 @Controller
 open class MyEntityController(
-        @Client("local") httpClient: HttpClient
+    @Client("local") httpClient: HttpClient
 ) {
     private val httpClient: BlockingHttpClient
 
@@ -29,26 +19,12 @@ open class MyEntityController(
         this.httpClient = httpClient.toBlocking()
     }
 
-    @Post("/test")
-    open fun test(@Body @NotNull @Valid animal: Animal): Mono<Animal> {
-        return Mono.just(animal)
-    }
-
-    @Get("/start")
-    open fun start() {
-        val bird = Bird(
-                numWings = 2,
-                beakLength = BigDecimal.valueOf(12, 1),
-                featherDescription = "Large blue and white feathers"
-        )
-        bird.color = ColorEnum.BLUE
-
-//        val animal = Animal(
-//                color = ColorEnum.RED,
-//                propertyClass = "ave"
-//        )
-
-        val resposne = httpClient.retrieve(HttpRequest.POST<Animal>("/test", bird), String::class.java)
-        println("!!!!! Response: $resposne")
+    @Post("/test{/apiVersion}")
+    open fun test(
+        @Header("X-Favor-Token") @Nullable xFavorToken: String? = null,
+        @PathVariable("apiVersion") @Nullable apiVersion: MyEnum? = MyEnum.V5,
+        @Header("Content-Type") @Nullable contentType: String? = "application/json"
+    ): String? {
+        return null
     }
 }
