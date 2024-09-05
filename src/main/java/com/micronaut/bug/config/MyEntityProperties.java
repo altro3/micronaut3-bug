ed236c32a5fc4197f9ee0ed54d17255786b714a1
altro3 <alexuyt@gmail.com>
2024-09-05 15:20:29 +0700

    Update libs

--- src/main/java/com/micronaut/bug/config/MyEntityProperties.java
+++ src/main/java/com/micronaut/bug/config/MyEntityProperties.java
@@ -2,20 +2,150 @@ package com.micronaut.bug.config;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
 import jakarta.annotation.PostConstruct;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.MonthDay;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.Period;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @Slf4j
-@Getter
-@Setter
+//@Getter
+//@Setter
+@ToString
 @ConfigurationProperties("app.entity")
 public class MyEntityProperties {
 
-    private String prop;
+    private Duration duration;
+    private Period period;
+    private YearMonth yearMonth;
+    private Year year;
+    private MonthDay monthDay;
+    private ZoneId zoneId;
+    private LocalDate localDate;
+    private LocalTime localTime;
+    private LocalDateTime localDateTime;
+    private ZonedDateTime zonedDateTime;
+    private OffsetDateTime offsetDateTime;
+    private OffsetTime offsetTime;
+    private Instant instant;
 
     @PostConstruct
     public void init() {
-//        log.info("prop = {}", prop);
+        log.info("config = {}", this);
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public Period getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
+    public YearMonth getYearMonth() {
+        return yearMonth;
+    }
+
+    public void setYearMonth(YearMonth yearMonth) {
+        this.yearMonth = yearMonth;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public MonthDay getMonthDay() {
+        return monthDay;
+    }
+
+    public void setMonthDay(MonthDay monthDay) {
+        this.monthDay = monthDay;
+    }
+
+    public ZoneId getZoneId() {
+        return zoneId;
+    }
+
+    public void setZoneId(ZoneId zoneId) {
+        this.zoneId = zoneId;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public LocalTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public ZonedDateTime getZonedDateTime() {
+        return zonedDateTime;
+    }
+
+    public void setZonedDateTime(ZonedDateTime zonedDateTime) {
+        this.zonedDateTime = zonedDateTime;
+    }
+
+    public OffsetDateTime getOffsetDateTime() {
+        return offsetDateTime;
+    }
+
+    public void setOffsetDateTime(OffsetDateTime offsetDateTime) {
+        this.offsetDateTime = offsetDateTime;
+    }
+
+    public OffsetTime getOffsetTime() {
+        return offsetTime;
+    }
+
+    public void setOffsetTime(OffsetTime offsetTime) {
+        this.offsetTime = offsetTime;
+    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+
+    public void setInstant(Instant instant) {
+        this.instant = instant;
     }
 }
--- src/main/java/com/micronaut/bug/controller/MyEntityController.java
+++ src/main/java/com/micronaut/bug/controller/MyEntityController.java
@@ -1,30 +1,25 @@
 package com.micronaut.bug.controller;
 
-import java.math.BigDecimal;
-
-import com.example.openapi.api.DefaultApi;
+import com.micronaut.bug.api.Animal;
+import com.micronaut.bug.api.Bird;
+import com.micronaut.bug.api.ColorEnum;
+import com.micronaut.bug.service.MyEntityService;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Head;
 import io.micronaut.http.annotation.Header;
-import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
-
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Mono;
 
-import com.micronaut.bug.api.Animal;
-import com.micronaut.bug.api.Bird;
-import com.micronaut.bug.api.ColorEnum;
-import com.micronaut.bug.service.MyEntityService;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 @ExecuteOn(TaskExecutors.BLOCKING)
 @Slf4j
@@ -33,36 +28,39 @@ public class MyEntityController {
 
     private final MyEntityService entityService;
     private final BlockingHttpClient httpClient;
-    private final DefaultApi defaultApi;
 
-    public MyEntityController(MyEntityService entityService, @Client("local") HttpClient httpClient, DefaultApi defaultApi) {
+    public MyEntityController(MyEntityService entityService, @Client("local") HttpClient httpClient) {
         this.entityService = entityService;
         this.httpClient = httpClient.toBlocking();
-        this.defaultApi = defaultApi;
     }
 
     @Head("/")
     void fileAvailable(
-            @Header("X-TEST-crc32") @NotNull String xtESTCrc32,
-            @Header("X-TEST-sha512") @NotNull String xtESTSha512,
-            @Header("X-TEST-size") @NotNull Long xtESTSize
+        @Header("X-TEST-crc32") @NotNull String xtESTCrc32,
+        @Header("X-TEST-sha512") @NotNull String xtESTSha512,
+        @Header("X-TEST-size") @NotNull Long xtESTSize
     ) {
         log.info("xtESTCrc32: {}", xtESTCrc32);
     }
 
     @Get("/test")
-    public void test() {
-        defaultApi.fileAvailable("test1", "test2", 123L);
+    public Optional<Integer> test() {
+        return Optional.of(100);
+    }
+
+    @Get("/test2")
+    public OptionalInt test2() {
+        return OptionalInt.of(100);
     }
 
     @Get("/start")
     public void start() {
 
         var bird = new Bird()
-                .numWings(2)
-                .beakLength(BigDecimal.valueOf(12, 1))
-                .featherDescription("Large blue and white feathers")
-                .color(ColorEnum.BLUE);
+            .numWings(2)
+            .beakLength(BigDecimal.valueOf(12, 1))
+            .featherDescription("Large blue and white feathers")
+            .color(ColorEnum.BLUE);
 
         var resposne = httpClient.retrieve(HttpRequest.<Animal>POST("/test", bird), Animal.class);
         log.info("Response: {}", resposne);
