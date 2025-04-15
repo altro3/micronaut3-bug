321c78fbfebd09c5e10d930dd4e812ce0c3aaf46
altro3 <alexuyt@gmail.com>
2025-04-15 16:44:11 +0700

    Update libs

--- src/main/java/com/micronaut/bug/controller/MyEntityController.java
+++ src/main/java/com/micronaut/bug/controller/MyEntityController.java
@@ -6,6 +6,7 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Head;
 import io.micronaut.http.annotation.Header;
+import io.micronaut.http.annotation.Part;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.scheduling.TaskExecutors;
@@ -14,12 +15,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
 
 @ExecuteOn(TaskExecutors.BLOCKING)
 @Slf4j
-@Controller
+@Controller("/api")
 public class MyEntityController {
 
     private final MyEntityService entityService;
@@ -51,4 +54,15 @@ public class MyEntityController {
     public void testMultipart(String fileName, CompletedFileUpload file) throws IOException {
         log.info("fileName: {}", fileName.getBytes());
     }
+
+    @Post(value = "/file", consumes = MediaType.MULTIPART_FORM_DATA)
+    public void endpoint2(@Part Map<String, CompletedFileUpload> files) {
+        log.info("endpoint2");
+    }
+
+    @Post(value = "/file2", consumes = MediaType.MULTIPART_FORM_DATA)
+    public void endpoint3(List<CompletedFileUpload> files) {
+        log.info("endpoint3");
+    }
+
 }
