d9d6d5437086f53ed0bd491ad5bcd4807deae267
altro3 <alexuyt@gmail.com>
2025-04-16 13:36:35 +0700

    Update libs

--- src/main/java/com/micronaut/bug/controller/MyEntityController.java
+++ src/main/java/com/micronaut/bug/controller/MyEntityController.java
@@ -1,33 +1,30 @@
 package com.micronaut.bug.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Slf4j
 @RestController
 public class MyEntityController {
 
     @PostMapping(value = "/testMultipart/{pathVar}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void testMultipart(WrappedRq rq) {
+    public void testMultipart(@Valid WrappedRq rq) {
         log.info("fileName: {}", rq.fileName.getBytes());
     }
 
     public record WrappedRq(
-        @PathVariable
         String pathVar,
-        @RequestParam
         String queryVar,
         // Part from multipart body
         String fileName,
         // Part from multipart body
+        @NotNull
         MultipartFile file
-    ) {}
+    ) {
+    }
 }
