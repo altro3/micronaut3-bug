4a5c2ffc9946488bc09ec14c6b179d28d60cc7c0
altro3 <alexuyt@gmail.com>
2025-05-22 16:22:57 +0700

    Test format

--- src/main/java/com/micronaut/bug/controller/MyEntityController.java
+++ src/main/java/com/micronaut/bug/controller/MyEntityController.java
@@ -1,12 +1,14 @@
 package com.micronaut.bug.controller;
 
 import com.micronaut.bug.service.MyEntityService;
+import io.micronaut.core.convert.format.Format;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Head;
 import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Part;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.scheduling.TaskExecutors;
@@ -17,8 +19,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.OptionalInt;
+
+import static io.micronaut.core.convert.converters.MultiValuesConverterFactory.FORMAT_CSV;
 
 @ExecuteOn(TaskExecutors.BLOCKING)
 @Slf4j
@@ -40,9 +43,9 @@ public class MyEntityController {
         log.info("xtESTCrc32: {}", xtESTCrc32);
     }
 
-    @Get("/test")
-    public Optional<Integer> test() {
-        return Optional.of(100);
+    @Get("/test/{param}/")
+    public void test(@PathVariable @Format(FORMAT_CSV) List<Integer> param) {
+        log.info("test");
     }
 
     @Get("/test2")
