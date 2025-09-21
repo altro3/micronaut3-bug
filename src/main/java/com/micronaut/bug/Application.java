d54fe61c7001970715c087a1abb94feb16d4977d
altro3 <alexuyt@gmail.com>
2025-09-21 14:11:54 +0700

    Update libs

--- src/main/java/com/micronaut/bug/Application.java
+++ src/main/java/com/micronaut/bug/Application.java
@@ -2,10 +2,15 @@ package com.micronaut.bug;
 
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.ApplicationContextConfigurer;
+import io.micronaut.context.ExecutionHandleLocator;
 import io.micronaut.context.annotation.ContextConfigurer;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.web.router.DefaultRouteBuilder;
+import io.micronaut.web.router.RouteBuilder;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -23,10 +28,26 @@ public class Application implements ApplicationContextConfigurer {
         builder.deduceEnvironment(false)
                 .banner(false)
                 .defaultEnvironments("local");
+
     }
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        var sss = Micronaut.run(Application.class, args);
+
+        sss.getBeansOfType(RouteBuilder.class).
     }
 
+    @Singleton
+    public class MyRoutes extends DefaultRouteBuilder { // (1)
+
+        public MyRoutes(ExecutionHandleLocator executionHandleLocator,
+                        UriNamingStrategy uriNamingStrategy) {
+            super(executionHandleLocator, uriNamingStrategy);
+        }
+
+        @Inject
+        void issuesRoutes() { // (2)
+            GET("/issues/show/{number}", (a, b) -> {}); // (3)
+        }
+    }
 }
