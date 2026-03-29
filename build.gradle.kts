3d31199500d0fa8b48ff0ab60d52609a0c9ab982
Altro <alexuyt@gmail.com>
2026-03-29 13:01:39 +0700

    Update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,17 @@ plugins {
 //    id("com.google.devtools.ksp") version "2.2.10-2.0.2"
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
     id("org.jetbrains.kotlin.kapt") version "1.9.25"
-    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
+//    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
 //    id "org.openapi.generator" version "7.19.0"
 }
 
 val ver = mapOf(
-    "micronaut" to "4.10.9",
+    "kotlin" to "1.9.25",
+    "micronaut" to "4.10.10",
     "core" to "4.10.18",
     "openapi" to "6.20.0",
     "serde" to "2.16.2",
+    "jackson" to "2.21.2",
 )
 
 //mainClassName = "com.micronaut.bug.Application"
@@ -45,28 +47,31 @@ dependencies {
 //    ksp("io.micronaut.openapi:micronaut-openapi")
 //    ksp("io.micronaut.security:micronaut-security-annotations")
 
-    kapt("io.micronaut.validation:micronaut-validation-processor")
+    kapt(platform("io.micronaut:micronaut-core-bom:${ver["core"]}"))
+    kapt("io.micronaut.validation:micronaut-validation-processor:${ver["core"]}")
 //    kapt("io.micronaut.serde:micronaut-serde-processor")
-    kapt("io.micronaut:micronaut-inject-java")
+    kapt("io.micronaut:micronaut-inject-java:${ver["core"]}")
     kapt("io.micronaut.openapi:micronaut-openapi:${ver["openapi"]}")
 //    kapt("io.micronaut.security:micronaut-security-annotations")
 
+    compileOnly("io.micronaut.openapi:micronaut-openapi:${ver["openapi"]}")
+//    compileOnly("jakarta.annotation:jakarta.annotation-api")
     compileOnly("io.micronaut:micronaut-inject-java")
     compileOnly("io.micronaut:micronaut-inject-kotlin")
-    compileOnly("io.micronaut.openapi:micronaut-openapi-annotations")
-    compileOnly("com.fasterxml.jackson.core:jackson-annotations")
+    compileOnly("io.micronaut.openapi:micronaut-openapi-annotations:${ver["openapi"]}")
+    compileOnly("com.fasterxml.jackson.core:jackson-databind:${ver["jackson"]}")
 
+    implementation(platform("io.micronaut:micronaut-core-bom:${ver["core"]}"))
     implementation("io.micronaut.validation:micronaut-validation")
     implementation("io.micronaut:micronaut-http-server-netty")
     implementation("io.micronaut:micronaut-http-client")
     implementation("io.micronaut:micronaut-management")
-    implementation("io.micronaut.data:micronaut-data-model")
     implementation("io.micronaut:micronaut-runtime")
     implementation("io.micronaut.reactor:micronaut-reactor")
     implementation("io.micronaut:micronaut-jackson-databind")
-//    implementation("io.micronaut.serde:micronaut-serde-jackson")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${ver["kotlin"]}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${ver["kotlin"]}")
 //    implementation("io.micronaut.security:micronaut-security-jwt")
 //    implementation("io.micronaut.security:micronaut-security-oauth2")
 
