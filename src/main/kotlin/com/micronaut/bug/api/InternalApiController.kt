d489ee03e04d612bd9e8093dc8489e32a917355b
Altro <alexuyt@gmail.com>
2026-05-02 12:09:39 +0700

    Victoria

--- src/main/kotlin/com/micronaut/bug/api/InternalApiController.kt
+++ src/main/kotlin/com/micronaut/bug/api/InternalApiController.kt
@@ -1,22 +1,26 @@
 package com.micronaut.bug.api
 
+import com.micronaut.bug.service.integration.extservice.ExtServiceClient
+import com.micronaut.bug.service.integration.extservice.api.MyDataRequest
+import com.micronaut.bug.service.integration.extservice.api.MyDataResponse
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class InternalApiController {
+class InternalApiController(
+    private val extServiceClient: ExtServiceClient,
+) {
 
     private val log = KotlinLogging.logger {}
 
     @GetMapping("/internal/ping")
-    fun process(): Map<String, String> {
+    fun process(): MyDataResponse? {
         // Здесь твоя common-либа должна автоматически подхватить rqId из MDC
         log.info { "== Service request == Received call from parent" }
 
-        return mapOf(
-            "status" to "OK",
-            "service" to "Thi is internal service"
-        )
+        val updateRs = extServiceClient.updateData(MyDataRequest(name = "Test item"))
+
+        return updateRs
     }
 }
--- src/main/kotlin/com/micronaut/bug/api/ServiceToServiceController.kt
+++ src/main/kotlin/com/micronaut/bug/api/ServiceToServiceController.kt
@@ -1,5 +1,6 @@
 package com.micronaut.bug.api
 
+import com.micronaut.bug.service.integration.extservice.ExtServiceClient
 import com.micronaut.bug.service.integration.internalservice.InternalServiceClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ServiceToServiceController(
     private val internalServiceClient: InternalServiceClient,
+    private val extServiceClient: ExtServiceClient,
 ) {
 
     @GetMapping("/service2service/ping")
-    fun ping(): Unit =
+    fun ping() {
         internalServiceClient.ping()
+        extServiceClient.ping()
+    }
 }
