53a523d556ab6f8c8430bcc5848d3ae72b042109
Altro <alexuyt@gmail.com>
2026-05-22 20:29:03 +0700

    Tracer optimize

--- src/main/kotlin/com/micronaut/bug/trace/config/TraceProperties.kt
+++ src/main/kotlin/com/micronaut/bug/trace/config/TraceProperties.kt
@@ -31,7 +31,6 @@ class TraceProperties(
         var useGzip: Boolean = true,
         var compressionThreshold: DataSize = DataSize.ofKilobytes(4),
         val maxSenders: Int = 5,
-
         val maxAttempts: Int = 3,
         val backoffMultiplier: Double = 2.0,
         val jitterMin: Double = 0.7,
