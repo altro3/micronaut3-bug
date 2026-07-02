b8bc92f795321346aae7bc979f48b1bead5466de
Altro <alexuyt@gmail.com>
2026-07-02 13:40:59 +0700

    My Llama

--- my-llama/src/tokenizer/bucket_queue.rs
+++ my-llama/src/tokenizer/bucket_queue.rs
@@ -2,10 +2,17 @@ pub struct BucketQueue;
 
 impl BucketQueue {
     #[inline(always)]
-    pub fn clear(buckets: &mut [u32], bitset: &mut [u64], next_node: &mut [u32], min_rank: &mut usize, max_rank_dirty: &mut usize, chunk_len: usize) {
-        if *min_rank <= *max_rank_dirty {
-            let start_bucket = *min_rank;
-            let end_bucket = (*max_rank_dirty).min(buckets.len() - 1);
+    pub fn clear(
+        buckets: &mut [u32],
+        bitset: &mut [u64],
+        next_node: &mut [u32],
+        min_rank: usize,
+        max_rank_dirty: usize,
+        chunk_len: usize,
+    ) -> (usize, usize) {
+        if min_rank <= max_rank_dirty {
+            let start_bucket = min_rank;
+            let end_bucket = max_rank_dirty.min(buckets.len() - 1);
             let count = (end_bucket - start_bucket) + 1;
 
             unsafe {
@@ -15,8 +22,14 @@ impl BucketQueue {
             let start_word = start_bucket >> 6;
             let end_word = end_bucket >> 6;
             let word_count = (end_word - start_word) + 1;
-            unsafe {
-                std::ptr::write_bytes(bitset.as_mut_ptr().add(start_word), 0x00, word_count * 8);
+
+            let bitset_ptr = bitset.as_mut_ptr();
+            let mut i = 0;
+            while i < word_count {
+                unsafe {
+                    *bitset_ptr.add(start_word + i) = 0;
+                }
+                i += 1;
             }
         }
 
@@ -25,8 +38,7 @@ impl BucketQueue {
             std::ptr::write_bytes(next_node.as_mut_ptr(), 0xFF, end_next);
         }
 
-        *min_rank = buckets.len();
-        *max_rank_dirty = 0;
+        (buckets.len(), 0)
     }
 
     #[inline(always)]
@@ -34,70 +46,111 @@ impl BucketQueue {
         buckets: &mut [u32],
         bitset: &mut [u64],
         next_node: &mut [u32],
-        min_rank: &mut usize,
-        max_rank_dirty: &mut usize,
+        min_rank: usize,
+        max_rank_dirty: usize,
         rank: u32,
         left_idx: usize,
-    ) {
+    ) -> (usize, usize) {
         let r = rank as usize;
-        if r < *min_rank {
-            *min_rank = r;
-        }
-        if r > *max_rank_dirty {
-            *max_rank_dirty = r;
-        }
+
+        let new_min = if r < min_rank { r } else { min_rank };
+        let new_max = if r > max_rank_dirty { r } else { max_rank_dirty };
+
+        let word_idx = r >> 6;
+        let bit_idx = r & 63;
+        let left_idx_u32 = left_idx as u32;
 
         unsafe {
             let head = *buckets.get_unchecked(r);
-            if head == left_idx as u32 {
-                return;
+            if head == left_idx_u32 {
+                return (new_min, new_max);
             }
 
             *next_node.get_unchecked_mut(left_idx) = head;
-            *buckets.get_unchecked_mut(r) = left_idx as u32;
-
-            let word_idx = r >> 6;
-            let bit_idx = r & 63;
+            *buckets.get_unchecked_mut(r) = left_idx_u32;
             *bitset.get_unchecked_mut(word_idx) |= 1 << bit_idx;
         }
+
+        (new_min, new_max)
     }
 
     #[inline(always)]
-    pub fn pop_packed(buckets: &mut [u32], bitset: &mut [u64], next_node: &mut [u32], min_rank: &mut usize) -> u64 {
+    pub fn pop_packed(buckets: &mut [u32], bitset: &mut [u64], next_node: &mut [u32], min_rank: usize) -> (u64, usize) {
         let buckets_len = buckets.len();
-        let mut curr_rank = *min_rank;
 
-        while curr_rank < buckets_len {
-            let word_idx = curr_rank >> 6;
-            let bit_offset = curr_rank & 63;
+        if min_rank >= buckets_len {
+            return (u64::MAX, min_rank);
+        }
 
-            unsafe {
-                let word = *bitset.get_unchecked(word_idx) & (!0u64 << bit_offset);
+        let mut word_idx = min_rank >> 6;
+        let bit_offset = min_rank & 63;
+        let bitset_len = bitset.len();
+        let bitset_ptr = bitset.as_ptr();
 
-                if word != 0 {
-                    let tz = word.trailing_zeros() as usize;
-                    let actual_rank = (word_idx << 6) + tz;
+        unsafe {
+            let mut word = *bitset_ptr.add(word_idx) & (!0u64 << bit_offset);
 
-                    let head = *buckets.get_unchecked(actual_rank);
-                    let head_idx = head as usize;
-                    let next = *next_node.get_unchecked(head_idx);
+            if word == 0 {
+                word_idx += 1;
 
-                    *buckets.get_unchecked_mut(actual_rank) = next;
-                    *next_node.get_unchecked_mut(head_idx) = u32::MAX;
+                while word_idx + 3 < bitset_len {
+                    let w0 = *bitset_ptr.add(word_idx);
+                    let w1 = *bitset_ptr.add(word_idx + 1);
+                    let w2 = *bitset_ptr.add(word_idx + 2);
+                    let w3 = *bitset_ptr.add(word_idx + 3);
 
-                    if next == u32::MAX {
-                        *bitset.get_unchecked_mut(word_idx) &= !(1 << tz);
+                    if (w0 | w1 | w2 | w3) != 0 {
+                        if w0 != 0 {
+                            word = w0;
+                            break;
+                        }
+                        if w1 != 0 {
+                            word = w1;
+                            word_idx += 1;
+                            break;
+                        }
+                        if w2 != 0 {
+                            word = w2;
+                            word_idx += 2;
+                            break;
+                        }
+                        word = w3;
+                        word_idx += 3;
+                        break;
+                    }
+                    word_idx += 4;
+                }
+
+                if word == 0 {
+                    while word_idx < bitset_len {
+                        word = *bitset_ptr.add(word_idx);
+                        if word != 0 {
+                            break;
+                        }
+                        word_idx += 1;
                     }
 
-                    *min_rank = actual_rank;
-                    return ((actual_rank as u64) << 32) | (head as u64);
+                    if word == 0 {
+                        return (u64::MAX, buckets_len);
+                    }
                 }
             }
 
-            curr_rank = (word_idx + 1) << 6;
-        }
+            let tz = word.trailing_zeros() as usize;
+            let actual_rank = (word_idx << 6) + tz;
 
-        *min_rank = buckets_len;
-        u64::MAX
+            let head = *buckets.get_unchecked(actual_rank);
+            let head_idx = head as usize;
+            let next = *next_node.get_unchecked(head_idx);
+
+            *buckets.get_unchecked_mut(actual_rank) = next;
+            *next_node.get_unchecked_mut(head_idx) = u32::MAX;
+
+            if next == u32::MAX {
+                *bitset.get_unchecked_mut(word_idx) &= !(1 << tz);
+            }
+
+            (((actual_rank as u64) << 32) | (head as u64), actual_rank)
+        }
     }
 }
