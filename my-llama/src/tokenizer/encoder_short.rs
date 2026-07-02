91650e1585eba15241217f772bb3012002eab3b8
Altro <alexuyt@gmail.com>
2026-07-02 12:41:33 +0700

    My Llama

--- my-llama/src/tokenizer/encoder_short.rs
+++ my-llama/src/tokenizer/encoder_short.rs
@@ -1,13 +1,70 @@
 use super::bpe_tokenizer::BpeTokenizer;
 use super::context::TokenizationContext;
 
-macro_rules! find_min_rank {
-    ($N:expr, $cached_ranks:expr, $min_rank:expr, $best_left:expr) => {
-        for i in 0..$N {
-            let rk = unsafe { *$cached_ranks.get_unchecked(i) };
-            if rk < $min_rank {
-                $min_rank = rk;
-                $best_left = i;
+macro_rules! generate_bpe_loop {
+    ($N:expr, $self:expr, $len:expr, $cached_ranks:expr, $cached_ids:expr, $prev:expr, $next:expr, $ids:expr, $token_count:expr, $slice:expr, $slice_len:expr) => {
+        loop {
+            let mut min_rank = u32::MAX;
+            let mut best_left = usize::MAX;
+
+            for i in 0..($N - 1) {
+                let rk = unsafe { *$cached_ranks.get_unchecked(i) };
+                if rk < min_rank {
+                    min_rank = rk;
+                    best_left = i;
+                }
+            }
+
+            if best_left == usize::MAX {
+                break;
+            }
+
+            let l = best_left;
+            let r = unsafe { *$next.get_unchecked(l) as usize };
+            let after_r = unsafe { *$next.get_unchecked(r) };
+
+            unsafe {
+                *$next.get_unchecked_mut(l) = after_r;
+                if (after_r as usize) < $len {
+                    *$prev.get_unchecked_mut(after_r as usize) = l as u8;
+                }
+                *$ids.get_unchecked_mut(l) = *$cached_ids.get_unchecked(l);
+            }
+
+            unsafe {
+                *$cached_ranks.get_unchecked_mut(l) = u32::MAX;
+                *$cached_ranks.get_unchecked_mut(r) = u32::MAX;
+            }
+
+            if (after_r as usize) < $len {
+                let id_l = unsafe { *$ids.get_unchecked(l) };
+                let id_after = unsafe { *$ids.get_unchecked(after_r as usize) };
+
+                let packed = $self.get_pair_packed(id_l, id_after);
+                if packed != u64::MAX {
+                    unsafe {
+                        *$cached_ranks.get_unchecked_mut(l) = (packed >> 32) as u32;
+                        *$cached_ids.get_unchecked_mut(l) = packed as u32;
+                    }
+                }
+            }
+
+            let before_l = unsafe { *$prev.get_unchecked(l) as usize };
+            if before_l < $len {
+                let id_before = unsafe { *$ids.get_unchecked(before_l) };
+                let id_l = unsafe { *$ids.get_unchecked(l) };
+
+                let packed = $self.get_pair_packed(id_before, id_l);
+                if packed != u64::MAX {
+                    unsafe {
+                        *$cached_ranks.get_unchecked_mut(before_l) = (packed >> 32) as u32;
+                        *$cached_ids.get_unchecked_mut(before_l) = packed as u32;
+                    }
+                } else {
+                    unsafe {
+                        *$cached_ranks.get_unchecked_mut(before_l) = u32::MAX;
+                    }
+                }
             }
         }
     };
@@ -71,86 +128,59 @@ impl BpeTokenizer {
             }
         }
 
-        loop {
-            let mut min_rank = u32::MAX;
-            let mut best_left = usize::MAX;
-
-            match len {
-                2 => find_min_rank!(2, cached_ranks, min_rank, best_left),
-                3 => find_min_rank!(3, cached_ranks, min_rank, best_left),
-                4 => find_min_rank!(4, cached_ranks, min_rank, best_left),
-                5 => find_min_rank!(5, cached_ranks, min_rank, best_left),
-                6 => find_min_rank!(6, cached_ranks, min_rank, best_left),
-                7 => find_min_rank!(7, cached_ranks, min_rank, best_left),
-                8 => find_min_rank!(8, cached_ranks, min_rank, best_left),
-                9 => find_min_rank!(9, cached_ranks, min_rank, best_left),
-                10 => find_min_rank!(10, cached_ranks, min_rank, best_left),
-                11 => find_min_rank!(11, cached_ranks, min_rank, best_left),
-                12 => find_min_rank!(12, cached_ranks, min_rank, best_left),
-                13 => find_min_rank!(13, cached_ranks, min_rank, best_left),
-                14 => find_min_rank!(14, cached_ranks, min_rank, best_left),
-                15 => find_min_rank!(15, cached_ranks, min_rank, best_left),
-                16 => find_min_rank!(16, cached_ranks, min_rank, best_left),
-                _ => unsafe { std::hint::unreachable_unchecked() },
-            }
-
-            if best_left == usize::MAX {
-                break;
-            }
-
-            let l = best_left;
-            let r = unsafe { *next.get_unchecked(l) as usize };
-            let after_r = unsafe { *next.get_unchecked(r) };
-
-            unsafe {
-                *next.get_unchecked_mut(l) = after_r;
-                if (after_r as usize) < len {
-                    *prev.get_unchecked_mut(after_r as usize) = l as u8;
-                }
-                *ids.get_unchecked_mut(l) = *cached_ids.get_unchecked(l);
-            }
-
-            unsafe {
-                *cached_ranks.get_unchecked_mut(l) = u32::MAX;
-                *cached_ranks.get_unchecked_mut(r) = u32::MAX;
-            }
-
-            if (after_r as usize) < len {
-                let id_l = unsafe { *ids.get_unchecked(l) };
-                let id_after = unsafe { *ids.get_unchecked(after_r as usize) };
-
-                let packed = self.get_pair_packed(id_l, id_after);
-                if packed != u64::MAX {
-                    unsafe {
-                        *cached_ranks.get_unchecked_mut(l) = (packed >> 32) as u32;
-                        *cached_ids.get_unchecked_mut(l) = packed as u32;
-                    }
-                }
-            }
-
-            let before_l = unsafe { *prev.get_unchecked(l) as usize };
-            if before_l < len {
-                let id_before = unsafe { *ids.get_unchecked(before_l) };
-                let id_l = unsafe { *ids.get_unchecked(l) };
-
-                let packed = self.get_pair_packed(id_before, id_l);
-                if packed != u64::MAX {
-                    unsafe {
-                        *cached_ranks.get_unchecked_mut(before_l) = (packed >> 32) as u32;
-                        *cached_ids.get_unchecked_mut(before_l) = packed as u32;
-                    }
-                } else {
-                    unsafe {
-                        *cached_ranks.get_unchecked_mut(before_l) = u32::MAX;
-                    }
-                }
-            }
-        }
-
-        let mut i = 0;
         let mut count = *token_count;
         let slice_len = slice.len();
 
+        match len {
+            2 => {
+                generate_bpe_loop!(2, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            3 => {
+                generate_bpe_loop!(3, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            4 => {
+                generate_bpe_loop!(4, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            5 => {
+                generate_bpe_loop!(5, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            6 => {
+                generate_bpe_loop!(6, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            7 => {
+                generate_bpe_loop!(7, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            8 => {
+                generate_bpe_loop!(8, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            9 => {
+                generate_bpe_loop!(9, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            10 => {
+                generate_bpe_loop!(10, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            11 => {
+                generate_bpe_loop!(11, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            12 => {
+                generate_bpe_loop!(12, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            13 => {
+                generate_bpe_loop!(13, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            14 => {
+                generate_bpe_loop!(14, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            15 => {
+                generate_bpe_loop!(15, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            16 => {
+                generate_bpe_loop!(16, self, len, cached_ranks, cached_ids, prev, next, ids, count, slice, slice_len);
+            }
+            _ => unsafe { std::hint::unreachable_unchecked() },
+        }
+
+        let mut i = 0;
         while i < len {
             if count >= slice_len {
                 break;
