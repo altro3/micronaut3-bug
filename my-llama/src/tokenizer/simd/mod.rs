46b8cdf04370776efeff995db925200107ae0cd1
Altro <alexuyt@gmail.com>
2026-07-02 17:42:10 +0700

    My Llama

--- my-llama/src/tokenizer/simd/mod.rs
+++ my-llama/src/tokenizer/simd/mod.rs
@@ -1,5 +1,4 @@
 pub mod avx2;
-pub mod fallback;
 pub mod macros;
 pub mod simd_splitter;
 pub mod utils;
--- my-llama/src/tokenizer/simd/simd_splitter.rs
+++ my-llama/src/tokenizer/simd/simd_splitter.rs
@@ -1,4 +1,4 @@
-use crate::tokenizer::simd::{avx2, fallback};
+use crate::tokenizer::simd::avx2;
 
 pub struct SimdSplitter;
 
@@ -11,6 +11,29 @@ impl SimdSplitter {
                 return unsafe { avx2::split(text, ids_buffer, byte_fallback) };
             }
         }
-        fallback::split(text, ids_buffer, byte_fallback)
+        Self::fallback_split(text, ids_buffer, byte_fallback)
+    }
+
+    fn fallback_split(text: &str, ids_buffer: &mut [u32], byte_fallback: &[u32; 256]) -> usize {
+        let bytes = text.as_bytes();
+        let len = bytes.len();
+        let mut tokens_found = 0;
+        let bytes_ptr = bytes.as_ptr();
+        let buf_len = ids_buffer.len();
+
+        for idx in 0..len {
+            let b = unsafe { *bytes_ptr.add(idx) };
+            let is_delim = if b <= 32 { ((1u64 << b) & 0x10000000600u64) != 0 } else { false };
+            if !is_delim {
+                if tokens_found >= buf_len {
+                    break;
+                }
+                unsafe {
+                    *ids_buffer.get_unchecked_mut(tokens_found) = *byte_fallback.get_unchecked(b as usize);
+                }
+                tokens_found += 1;
+            }
+        }
+        tokens_found
     }
 }
