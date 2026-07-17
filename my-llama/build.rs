3d1d8f1cdd940d7b509abf29e23eb6d8bbb15ee3
Altro <alexuyt@gmail.com>
2026-07-17 16:34:48 +0700

    Kernels

--- my-llama/build.rs
+++ my-llama/build.rs
@@ -1,53 +1,22 @@
-use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=cuda/src");
-    println!("cargo:rerun-if-changed=cuda/include");
+    // Cargo будет перезапускать этот скрипт только при изменении исходников CUDA в новой папке backends
+    println!("cargo:rerun-if-changed=backends/cuda/src");
+    println!("cargo:rerun-if-changed=backends/cuda/include");
 
     let cuda_path = PathBuf::from(r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.3");
-
     let cuda_lib_dir = cuda_path.join("lib").join("x64");
-    let cuda_bin_dir = cuda_path.join("bin");
 
+    // Пути поиска для линковщика Rust (системные динамические либы NVIDIA)
     println!("cargo:rustc-link-search=native={}", cuda_lib_dir.display());
-
     println!("cargo:rustc-link-lib=dylib=cudart");
     println!("cargo:rustc-link-lib=dylib=cublas");
     println!("cargo:rustc-link-lib=dylib=cublasLt");
 
-    let nvcc_path = cuda_bin_dir.join("nvcc.exe");
-    unsafe {
-        env::set_var("NVCC", nvcc_path);
-    }
+    // Забираем нашу скомпилированную батником статическую библиотеку ядер (те самые 2.5 МБ)
+    let compiled_lib_dir = PathBuf::from(r"D:\.b_llama\backends\cuda\Release");
 
-    let mut build = cc::Build::new();
-    build
-        .cuda(true)
-        .flag("-gencode=arch=compute_120,code=sm_120")
-        .flag("-gencode=arch=compute_120,code=compute_120")
-        .flag("-gencode=arch=compute_90,code=sm_90")
-        .flag("-gencode=arch=compute_89,code=sm_89")
-        .flag("-O3")
-        .flag("-std=c++20")
-        .flag("--use_fast_math")
-        .include("cuda/include");
-
-    let target = env::var("TARGET").unwrap();
-    if target.contains("msvc") {
-        build.flag("-Xcompiler").flag("/EHsc");
-    }
-
-    let paths = fs::read_dir("cuda/src")
-        .expect("Не удалось прочитать директорию cuda/src")
-        .filter_map(|entry| entry.ok())
-        .map(|entry| entry.path())
-        .filter(|path| path.is_file() && path.extension().is_some_and(|ext| ext == "cu"));
-
-    for path in paths {
-        build.file(path);
-    }
-
-    build.compile("cuda_kernels");
+    println!("cargo:rustc-link-search=native={}", compiled_lib_dir.display());
+    println!("cargo:rustc-link-lib=static=cuda_kernels");
 }
--- my-llama/src/lib.rs
+++ my-llama/src/lib.rs
@@ -4,13 +4,13 @@ pub mod sys;
 pub mod tokenizer;
 pub mod utils;
 
-unsafe extern "C" {
-    pub fn init_cublas_infrastructure();
-}
+// unsafe extern "C" {
+//     pub fn init_cublas_infrastructure();
+// }
 
 pub fn init_framework() {
-    unsafe {
-        init_cublas_infrastructure();
-    }
+    // unsafe {
+    //     init_cublas_infrastructure();
+    // }
     println!("[MY-LLAMA] Высокопроизводительный движок на базе асинхронной архитектуры арен успешно запущен.");
 }
