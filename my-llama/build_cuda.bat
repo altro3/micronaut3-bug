e73a8173e74015a803e368797039b27428adb319
Altro <alexuyt@gmail.com>
2026-07-17 13:54:20 +0700

    Kernels

--- my-llama/build_cuda.bat
+++ my-llama/build_cuda.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Определение путей
 set "RAW_SRC=%~dp0"
 if "!RAW_SRC:~-1!"=="\" set "RAW_SRC=!RAW_SRC:~0,-1!"
 
@@ -21,7 +20,7 @@ if not exist "!CUDA_PATH!" (
     exit /b 1
 )
 
-:: Если передан аргумент "clean", то удаляем папку целиком (включая скачанный CUTLASS)
+:: Режим тотальной очистки (вызывается вручную как build_cuda.bat clean)
 if "%1"=="clean" (
     echo [INFO] Performing TOTAL clean wipe as requested...
     if exist "!BUILD_DIR!" rmdir /s /q "!BUILD_DIR!"
@@ -30,14 +29,10 @@ if "%1"=="clean" (
 if not exist "!BUILD_DIR!" mkdir "!BUILD_DIR!"
 cd /d "!BUILD_DIR!"
 
-:: мы удаляем ТОЛЬКО кэш переменных CMakeCache.txt и файлы сборки твоих собственных ядер.
-echo [INFO] Refreshing CMake cache...
+:: Удаляем ТОЛЬКО файл кэша CMakeCache.txt, чтобы CMake видел
+:: добавление новых файлов в структуру, но НЕ трогаем папки с .obj файлами!
+echo [INFO] Refreshing CMake configuration...
 if exist CMakeCache.txt del /f /q CMakeCache.txt
-
-:: Удаляем старые объектные файлы твоих ядер, чтобы гарантировать их пересборку,
-:: но не трогаем служебную подпапку _deps, где лежит скачанный CUTLASS.
-if exist backends\cuda\cuda_kernels.dir rmdir /s /q backends\cuda\cuda_kernels.dir
-if exist backends\cuda\Release rmdir /s /q backends\cuda\Release
 
 echo [INFO] Running CMake configuration...
 cmake -T "cuda=!CUDA_PATH!" -DBACKEND=CUDA "!SRC_DIR!"
@@ -47,7 +42,8 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-echo [INFO] Compiling static library (Fast Incremental Mode)...
+:: Нативная проверка изменений от MSBuild. Сборка пойдет на 16 ядрах.
+echo [INFO] Compiling static library (Native Incremental Mode)...
 cmake --build . --config Release --parallel 16
 
 if %errorlevel% neq 0 (
