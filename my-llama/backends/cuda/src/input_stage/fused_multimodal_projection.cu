8ef0dd5a6fea14676a50e4f771a24e9ac6bc218f
Altro <alexuyt@gmail.com>
2026-07-19 23:29:20 +0700

    Fused multimodal projection

--- my-llama/backends/cuda/src/input_stage/fused_multimodal_projection.cu
+++ my-llama/backends/cuda/src/input_stage/fused_multimodal_projection.cu
@@ -15,12 +15,12 @@ extern "C" void launch_fused_multimodal_projection(
     const void * __restrict__ weight_matrix,
     const float * __restrict__ bias,
     const int32_t * __restrict__ vision_segments,
-    int32_t num_segments,
+    const int32_t num_segments,
     const int32_t vision_hidden_size,
     const int32_t text_hidden_size,
     const int32_t tp_rank,
     const int32_t tp_size,
-    cudaStream_t stream
+    const cudaStream_t stream
 ) {
     const int32_t local_out_features = text_hidden_size / tp_size;
     const int32_t rank_offset_out_features = tp_rank * local_out_features;
@@ -97,7 +97,7 @@ extern "C" void launch_fused_multimodal_projection(
     cudaMemcpyAsync(host_segments, vision_segments, num_segments * 2 * sizeof(int32_t), cudaMemcpyDeviceToHost, stream);
     cudaStreamSynchronize(stream);
 
-    using ProblemShapeType = Shape<int, int, int>;
+    using ProblemShapeType = cutlass::gemm::GemmCoord;
     const auto host_problem_shapes = new ProblemShapeType[num_segments];
 
     const auto host_ptr_A = new ElementA *[num_segments];
@@ -108,9 +108,9 @@ extern "C" void launch_fused_multimodal_projection(
     for (int32_t i = 0; i < num_segments; ++i) {
         const int32_t start_token = host_segments[i * 2];
         const int32_t end_token = host_segments[i * 2 + 1];
-        int32_t segment_tokens = end_token - start_token;
+        const int32_t segment_tokens = end_token - start_token;
 
-        host_problem_shapes[i] = make_shape(segment_tokens, local_out_features, vision_hidden_size);
+        host_problem_shapes[i] = ProblemShapeType(segment_tokens, local_out_features, vision_hidden_size);
 
         host_ptr_A[i] = const_cast<ElementA *>(static_cast<const ElementA *>(input_tokens)) + start_token * vision_hidden_size;
         host_ptr_B[i] = const_cast<ElementB *>(static_cast<const ElementB *>(weight_matrix)) + rank_offset_out_features * vision_hidden_size;
@@ -138,7 +138,6 @@ extern "C" void launch_fused_multimodal_projection(
 
     GemmGroupedUniversal::Arguments arguments;
 
-
     arguments.mode = cutlass::gemm::GemmUniversalMode::kGrouped;
     arguments.batch_count = num_segments;
 
@@ -151,6 +150,11 @@ extern "C" void launch_fused_multimodal_projection(
     arguments.ldb = vision_hidden_size;
     arguments.ldc = local_out_features;
     arguments.ldd = local_out_features;
+
+    arguments.stride_a = vision_hidden_size;
+    arguments.stride_b = vision_hidden_size;
+    arguments.stride_c = local_out_features;
+    arguments.stride_d = local_out_features;
 
     arguments.epilogue.thread.op_0.op_0 = {};
     arguments.epilogue.thread.op_0.op_1.ptr_col = const_cast<float *>(bias) + rank_offset_out_features;
